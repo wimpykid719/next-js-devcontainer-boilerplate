d6a3e083ae905ea4cd143e1c46c68e26efabe821
“wataru” <ltomu.web@gmail.com>
2025-11-14 12:32:51 +0900

    feat: Next.js 16対応
    Biomeがデフォルトでインストールされるように変更しました

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -3,7 +3,17 @@
 # package.json がなければ pnpm create create-next-app を実行
 if [ ! -f "/workspace/app/package.json" ]; then
   echo "No package.json found, initializing Next.js project..."
-  pnpm create create-next-app@latest app --typescript --tailwind --eslint --app --src-dir --import-alias "@/*" --turbopack -y
+  pnpm create next-app@latest app \
+  --typescript \
+  --tailwind \
+  --biome \
+  --app \
+  --src-dir \
+  --import-alias "@/*" \
+  --turbopack \
+  --react-compiler \
+  --yes
+
   rm -rf /workspace/app/.gitignore
 else
   echo "package.json found, skipping Next.js project initialization."
